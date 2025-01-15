ab92844c3d4481de22906b54fe888b3d92e79f92
Juan Galdo <juan.galdo@ucb.edu.bo>
2025-01-15 11:08:04 -0400

    Creating my own pipe and learning library for dates

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Product } from '../../../shared/models/product.model';
+import { Product } from '@shared/models/product.model';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
