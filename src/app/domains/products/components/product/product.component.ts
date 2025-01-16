4b4f41c9a65aea97a5b21a72593202bdb1410298
Juan Galdo <juan.galdo@ucb.edu.bo>
2025-01-15 23:21:13 -0400

    New changes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,13 @@
-import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '@shared/models/product.model';
 import { ReversePipe } from '@shared/pipes/reverse.pipe';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule, ReversePipe, TimeAgoPipe],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe, RouterLinkWithHref],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
@@ -16,7 +17,6 @@ export class ProductComponent {
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler() {
-    console.log('click from child');
     this.addToCart.emit(this.product);
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,8 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '@product/components/product/product.component';
 import { Product } from '@shared/models/product.model';
+import { Category } from '@shared/models/category.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
 
 @Component({
   selector: 'app-list',
@@ -13,11 +15,22 @@ import { ProductService } from '@shared/services/product.service';
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
   cart = this.cartService.cart;
 
   ngOnInit() {
+    this.getProduct();
+    this.getCategories();
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProduct() {
     this.productService.getProduct().subscribe({
       next: (product) => {
         this.products.set(product);
@@ -28,7 +41,14 @@ export class ListComponent {
     });
   }
 
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getCategories().subscribe({
+      next: (category) => {
+        this.categories.set(category);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
 }
